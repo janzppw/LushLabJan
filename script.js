14b0861df11b153671ef35da34c7e74b9c4babeb
janzppw <717824f123@kce.ac.in>
2025-04-23 08:17:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function handleBuy(productName) {
+  alert(`View more details for ${productName}`);
+}
+
+function handleSubscribe() {
+  const email = document.querySelector('input[type="email"]').value;
+  if (email) {
+    alert(`Thanks for subscribing, ${email}!`);
+  } else {
+    alert("Please enter valid Email!");
+  }
+}
